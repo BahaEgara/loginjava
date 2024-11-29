577b39be5a4cb4c3cd82f698bcd8a649b08e5b8f
BahaEgara <131773831+BahaEgara@users.noreply.github.com>
2024-11-29 17:46:28 +0300

    Add files via upload

---
+++ PharmacyProject/src/com/pharma/Main.java
@@ -0,0 +1,19 @@
+
+package com.pharma;
+
+import javax.swing.*;
+
+public class Main {
+    public static void main(String[] args) {
+        JFrame frame = new JFrame();
+        frame.setSize(150,600);
+        SwingUtilities.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+                LandingPage landingPage = new LandingPage();
+
+            }
+        });
+    }
+}
+
